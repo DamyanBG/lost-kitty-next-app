bffe8be4d62cbc31cc26824d2404e3214585830f
DamyanBG <d.dimitrov.1988@abv.bg>
2023-12-28 19:08:34 +0200

    favicon and bigger links for pagination

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import Header from '../components/header/header'
+import Footer from '@/components/footer/footer'
 import AuthProvider from '@/context/AuthProvider'
 
 import './globals.css'
 import 'boxicons/css/boxicons.min.css'
-import Footer from '@/components/footer/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
